88c3199b6f500993647afed2275bf45002ac0a9d
DhirajOfficial <122342848+DhirajsAmbekar@users.noreply.github.com>
2024-11-23 18:21:18 +0530

    Update index.js

--- chat-ba/index.js
+++ chat-ba/index.js
@@ -1,38 +1,12 @@
 import express from "express";
-import cors from "cors";
-import { Server } from "socket.io";
-import { createServer } from "http";
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-const server = createServer(app);
+const port = 3000;
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+app.get("/dhiraj", (req, res) => {
+  res.send("Hello World!");
 });
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Basic route
-app.get("/", (req, res) => {
-  res.send("Chat API is running");
-});
-
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
-  socket.emit("welcome", `Welcome to the chat, ${socket.id}`);
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
-
-// Change app.listen to server.listen since we're using Socket.IO
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
